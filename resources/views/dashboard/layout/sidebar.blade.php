3d6252658238a59b70ca40373218cf1f850e08ae
kukuh2307 <kukuhagung12@gmail.com>
2023-09-01 22:20:47 +0700

    (add) fungsi hapus artikel dan relasi tabel yaitu artikel_tag -11:16.43

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -5,6 +5,6 @@
         <a class="list-group-item list-group-item-action list-group-item-light p-3 {{ $tittle =='About' ? 'active' : '' }}" href="/dashboard">About</a>
         <a class="list-group-item list-group-item-action list-group-item-light p-3 {{ $tittle =='Slide' ? 'active' : '' }}" href="/dashboard/slide">Slide</a>
         <a class="list-group-item list-group-item-action list-group-item-light p-3 {{ $tittle =='Kategori' ? 'active' : '' }}" href="/dashboard/kategori">Kategori</a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3 {{ $tittle =='Artikel' ? 'active' : '' }}" href="/dashboard/artikel/create">Artikel</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3 {{ $tittle =='Artikel' ? 'active' : '' }}" href="/dashboard/artikel">Artikel</a>
     </div>
 </div>
