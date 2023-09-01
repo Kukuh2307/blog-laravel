1f8c4f071ccd8d29a6e72b96d0b6d128f2de629c
kukuh2307 <kukuhagung12@gmail.com>
2023-09-01 23:58:24 +0700

    (add and modify) fungsi read,delete dan update artikel -11

--- resources/views/dashboard/layout/main.blade.php
+++ resources/views/dashboard/layout/main.blade.php
@@ -18,8 +18,6 @@
         <link href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" type="text/css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/css/bootstrap-tokenfield.css">
 
-        {{-- tokenfield local --}}
-        <link rel="stylesheet" href="{{ asset('tokenfield') }}/tokenfield.css">
 
         {{-- cdn font awesome --}}
         <link rel="stylesheet" 
@@ -54,13 +52,14 @@
         {{-- tokenfield js --}}
         <script type="text/javascript" src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield.js"></script>
-        <script src="{{ asset('tokenfield') }}/tokenfield.js"></script>
 
         {{-- cdn jquery untuk generate slug--}}
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
         {{-- datatable --}}
         <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
+        
 
         <script>
                 // fungsi untuk menampilkan gambar saat admin menginputkan gambar baru
@@ -78,20 +77,18 @@
                         tampilGambar.src = oFREvent.target.result;
                 };
             }
-        </script>
 
-        <script>
             ClassicEditor
             .create( document.querySelector( '#editor' ) )
             .catch( error => {
                 console.error( error );
             } );
 
-            data table
-            $(document).ready(function(){
-                $('#myTable').DataTable();
-            });
-            // new DataTable('#myTable');
+            // data table
+            // $(document).ready(function(){
+            //     $('#myTable').DataTable();
+            // });
+            new DataTable('#myTable');
         </script>
 
         <script>
