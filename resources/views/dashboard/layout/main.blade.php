4fb9058937d1286ad758a7fe588910abc672d860
kukuh2307 <kukuhagung12@gmail.com>
2023-09-01 21:07:21 +0700

    (not solve) tokenfield tambah artikel baru

--- resources/views/dashboard/layout/main.blade.php
+++ resources/views/dashboard/layout/main.blade.php
@@ -14,6 +14,13 @@
         {{-- css datatable --}}
         <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 
+        {{-- bootstrap tokenfield --}}
+        <link href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/css/bootstrap-tokenfield.css">
+
+        {{-- tokenfield local --}}
+        <link rel="stylesheet" href="{{ asset('tokenfield') }}/tokenfield.css">
+
         {{-- cdn font awesome --}}
         <link rel="stylesheet" 
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
@@ -44,6 +51,11 @@
         <!-- Core theme JS-->
         <script src="{{ asset('admin') }}/js/scripts.js"></script>
 
+        {{-- tokenfield js --}}
+        <script type="text/javascript" src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield.js"></script>
+        <script src="{{ asset('tokenfield') }}/tokenfield.js"></script>
+
         {{-- cdn jquery untuk generate slug--}}
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
@@ -51,33 +63,47 @@
         <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
         <script>
-            // fungsi untuk menampilkan gambar saat admin menginputkan gambar baru
-            function tampilGambar() {
-                let gambarInput = document.getElementById('gambar');
-                let tampilGambar = document.querySelector('.tampil-gambar');
+                // fungsi untuk menampilkan gambar saat admin menginputkan gambar baru
+                function tampilGambar() {
+                    let gambarInput = document.getElementById('gambar');
+                    let tampilGambar = document.querySelector('.tampil-gambar');
 
-                tampilGambar.style.display = 'block';
+                    tampilGambar.style.display = 'block';
 
-                const oFReader = new FileReader();
-    
-                oFReader.readAsDataURL(gambarInput.files[0]);
+                    const oFReader = new FileReader();
+        
+                    oFReader.readAsDataURL(gambarInput.files[0]);
 
-                oFReader.onload = function(oFREvent) {
-                    tampilGambar.src = oFREvent.target.result;
-            };
-        }
+                    oFReader.onload = function(oFREvent) {
+                        tampilGambar.src = oFREvent.target.result;
+                };
+            }
+        </script>
 
-        ClassicEditor
-		.create( document.querySelector( '#editor' ) )
-		.catch( error => {
-			console.error( error );
-		} );
+        <script>
+            ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
+            } );
 
-        // data table
-        // $(document).ready(function(){
-        //     $('#myTable').DataTable();
-        // });
-        new DataTable('#myTable');
+            data table
+            $(document).ready(function(){
+                $('#myTable').DataTable();
+            });
+            // new DataTable('#myTable');
+        </script>
+
+        <script>
+            // token file tambah tag artikel baru
+            $('#tokenfield').tokenfield({
+                autocomplete: {
+                source: [],
+                delay: 100,
+                prefilled: []
+            },
+            showAutocompleteOnFocus: true
+            })
         </script>
     </body>
 </html>
